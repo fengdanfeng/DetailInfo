fcdb89e6c13c3a83920a8e739eb1e96472350ed0
fengdanfeng <937326389@qq.com>
2015-08-16 18:37:06 +0800

    firstcommit

---
+++ js/common.js
@@ -0,0 +1,12 @@
+(function() {
+  window.onload = function() {
+    if(document.documentElement.scrollHeight <= document.documentElement.clientHeight) {
+          var bodyTag = document.getElementsByTagName('body')[0];
+          bodyTag.style.height = document.documentElement.clientWidth / screen.width * screen.height + 'px';
+        }
+        //setTimeout(function() {
+        //  window.scrollTo(0, 1)
+        //}, 0);
+  };
+
+}).call(this);
